1c7734c7900695f8281328986e8b2a932ea2767b
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-13 14:17:49 +0000

    create and render FunctionalGreetings and Functional GreetingsWithProps components

---
+++ my-app/src/components/FunctionalGreetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from Kay!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from me again!</h1>;
+
+export default FunctionalGreeting;
