fc0bd5570eef27183e272a4f5bc7b64e0958e7e0
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-14 16:35:30 +0000

    create CondidtionalRendering.. components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
-import EventBinding from "./components/EventBinding";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
 	return (
 		<div className="App">
-			<EventBinding />
+			<ConditionalRenderingClass />
+			<ConditionalRenderingFunctional connected={false} />
 		</div>
 	);
 }
