3853ffb19ee60e8ebfb6214d2e44047bc60b7793
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-14 20:20:22 +0000

    create components to test changing parent state from child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
 	return (
 		<div className="App">
-			<NestingComponents />
+			<MethodsAsPropsParent />
 		</div>
 	);
 }
