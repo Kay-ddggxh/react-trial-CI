959bf3b10c43e4a6c47c8c5801a4dd37c1519a09
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-21 10:47:40 +0000

    add component to render book of lists with map method

---
+++ my-app/src/components/RenderingLists.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Book from "./Book";
+
+function RenderingLists() {
+	const bookList = ["to kill a mockingbird", "the expanse", "hamlet"];
+
+	const books = [
+		{
+			title: "expanse",
+			author: "someone cool",
+			pages: 234,
+		},
+		{
+			title: "harry potter",
+			author: "terf bag",
+			pages: 154,
+		},
+		{
+			title: "lord of the rings",
+			author: "old man",
+			pages: 684,
+		},
+	];
+	return (
+		<div>
+			{bookList.map((book) => {
+				return <h2>{book}</h2>;
+			})}
+			<hr />
+			{books.map((book) => {
+				return (
+					<Book book={book}/>
+				);
+			})}
+		</div>
+	);
+}
+
+export default RenderingLists;
