8f8eb3697e9cb1ecbd91745154913eac3fca6c28
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-21 10:58:10 +0000

    add key for book to RenderingLists component

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -24,13 +24,11 @@ function RenderingLists() {
 	return (
 		<div>
 			{bookList.map((book) => {
-				return <h2>{book}</h2>;
+				return <h2 key={book}>{book}</h2>;
 			})}
 			<hr />
 			{books.map((book) => {
-				return (
-					<Book book={book}/>
-				);
+				return <Book key={book.title} book={book} />;
 			})}
 		</div>
 	);
