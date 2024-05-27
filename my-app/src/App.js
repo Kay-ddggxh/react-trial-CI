f4a7078bf266f54873931e04dc05bf9ac0e29c3d
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-27 14:57:19 +0000

    create components to test CompWillUnmount method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,11 +7,12 @@ import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 import RenderingLists from "./components/RenderingLists";
 import LifeCyclesCDM from "./components/LifeCyclesCDM";
 import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
 	return (
 		<div className="App">
-			<LifeCyclesCDU />
+			<LifeCyclesCWU />
 		</div>
 	);
 }
