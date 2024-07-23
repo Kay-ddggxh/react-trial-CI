21258b98be4f3ea58737251bc0be0339fdbb7355
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-07-23 08:03:48 +0000

    add controlled form fields for select options and textarea

--- my-app/src/components/ControlledForms.js
+++ my-app/src/components/ControlledForms.js
@@ -6,6 +6,8 @@ export class ControlledForms extends Component {
 
 		this.state = {
 			name: "",
+			category: "website",
+			comments: "",
 		};
 	}
 
@@ -15,18 +17,60 @@ export class ControlledForms extends Component {
 		});
 	};
 
+	handleCategoryChange = (event) => {
+		this.setState({
+			category: event.target.value,
+		});
+	};
+
+	handleCommentsChange = (event) => {
+		this.setState({
+			comments: event.target.value,
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+	};
+
 	render() {
 		return (
 			<div>
-				<form>
-					<label htmlFor="id-name">Your name:</label>
-					<input
-						value={this.state.name}
-						onChange={this.handleNameChange}
-						id="id-name"
-						name="name"
-						type="text"
-					/>
+				<h2>Please fill out the form below:</h2>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label htmlFor="id-name">Your name:</label>
+						<input
+							value={this.state.name}
+							onChange={this.handleNameChange}
+							id="id-name"
+							name="name"
+							type="text"
+						/>
+					</div>
+					<div>
+						<label htmlFor="id-category">Query category:</label>
+						<select
+							id="id-category"
+							name="category"
+							value={this.state.category}
+							onChange={this.handleCategoryChange}
+						>
+							<option value="website">Website issue</option>
+							<option value="order">Order issue</option>
+							<option value="general">General inquiry</option>
+						</select>
+					</div>
+					<div>
+						<label htmlFor="id-comments">Comments:</label>
+						<textarea
+							id="id-comments"
+							name="comments"
+							value={this.state.comments}
+							onChange={this.handleCommentsChange}
+						/>
+					</div>
 					<input type="submit" value="Submit" />
 				</form>
 			</div>
